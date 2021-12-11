866c72071f359add8cbbcca6d4e445d8d8a790c9
Kidus <kidusyo531@gmail.com>
2021-12-10 19:24:49 -0700

    implemented backpropagation

--- Neural Networks/nn.py
+++ Neural Networks/nn.py
@@ -42,6 +42,7 @@ def main():
 
     #print(weights)
 
+
     # forward pass (calculate all the z values)
     nn_width = 2
     z_values = []
@@ -54,8 +55,8 @@ def main():
             prev_layer = layer - 1
             input = z_values[prev_layer]
             node_weight = []
-            for l in range(input_width):
-                index = (prev_layer * nn_width * input_width) + (nn_width * l) + node_index
+            for i in range(input_width):
+                index = (prev_layer * nn_width * input_width) + (nn_width * i) + node_index
                 node_weight.append(weights[index][weight_value])
            
             z = sigmoid(dot(input, node_weight))
@@ -67,14 +68,69 @@ def main():
     prev_layer = num_of_layers - 1
     input = z_values[prev_layer]
     node_weight = []
-    for l in range(input_width):
-        index = (prev_layer * nn_width * input_width) + l
+    for i in range(input_width):
+        index = (prev_layer * nn_width * input_width) + i
         node_weight.append(weights[index][weight_value])
     
     y = dot(input, node_weight)
     print(y)
 
+
     # backpropagation
+    label = 1
+    partial_l_y = y - label
+    top_layer = num_of_layers - 1 # (2)
+    partial_derivatives = []
+    for layer in reversed(range(1, num_of_layers + 1)):
+        # top layer (3)
+        if layer == num_of_layers: 
+            for i in range(input_width):
+                #index = (prev_layer * nn_width * input_width) + i
+                #print(weights[index])
+                input = z_values[top_layer]
+                partial_y_w = input[i]
+                partial_derivatives.append(partial_l_y * partial_y_w)
+
+            continue
+        
+        # layer 2
+        if layer == 2:
+            for node_index in range(nn_width):
+                next_layer = layer - 1
+                for i in range(input_width):
+                    index = (top_layer * nn_width * input_width) + node_index + 1
+                    #print(weights[index])
+                    partial_y_z = weights[index][weight_value]
+                    node_to = z_values[top_layer][node_index + 1]
+                    node_from = z_values[next_layer][i]
+                    partial_z_w = node_to * (1 - node_to) * node_from
+                    partial_derivatives.append(partial_l_y * partial_y_z * partial_z_w)
+            
+            continue
+        
+
+        # layer 1
+        if layer == 1:
+            for node_index in range(nn_width):
+                prev_layer = layer - 1
+                for i in range(input_width):
+                    sum = 0
+                    for node_path_index in range(nn_width):
+                        #print(node_index, i, node_path_index)
+                        index = (top_layer * nn_width * input_width) + node_path_index + 1 
+                        #print(weights[index])
+                        partial_y_z = weights[index][weight_value]
+                        #index = (layer * nn_width * input_width) + node_index + node_path_index + 1 
+                        index = (layer * nn_width * input_width) + (nn_width * (i + 1)) + node_index + node_path_index
+                        partial_z_z = weights[index][weight_value]
+                        node_to = z_values[layer][node_index + 1]
+                        node_from = z_values[prev_layer][i]
+                        partial_z_w = node_to * (1 - node_to) * node_from
+                        sum += partial_l_y * partial_y_z * partial_z_z * partial_z_w
+                    partial_derivatives.append(sum)
+
+        print(partial_derivatives)
+        #print(len(partial_derivatives))
 
 
 
