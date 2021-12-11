2bab4956303ddf5d9ec2b8041d150101b28d381a
Kidus <kidusyo531@gmail.com>
2021-12-11 13:51:54 -0700

    implemented the sgd algorithm using nn but its not returning accurate weights

--- Neural Networks/nn.py
+++ Neural Networks/nn.py
@@ -52,7 +52,7 @@ def forward_pass(x, weights, num_of_layers, nn_width, input_width, question):
     z_values.append(x)
     for layer in range(1, num_of_layers):
         z_values.append([1]) # the first element is the bias (1)
-        for node_index in range(nn_width):
+        for node_index in range(input_width - 1):
             prev_layer = layer - 1
             input = z_values[prev_layer]
             node_weight = []
@@ -87,6 +87,8 @@ def back_propagation(x, weights, label, nn_width, input_width, question):
 
     # first forward pass (calculate all the z values)
     z_values, y = forward_pass(x, weights, num_of_layers, nn_width, input_width, question)
+
+    #print(z_values)
 
     partial_l_y = y - label
     top_layer = num_of_layers - 1 # (2)
@@ -146,6 +148,7 @@ def back_propagation(x, weights, label, nn_width, input_width, question):
 
 # neural network algorithm using stochastic gradient descent
 def nn(data, w, lr, t, a, nn_width, input_width, question):
+    weight_value = 3
     for epoch in range(t):
         # update learning rate so weight is ensured to converge
         r = lr / (1 + (lr * epoch / a))
@@ -154,7 +157,7 @@ def nn(data, w, lr, t, a, nn_width, input_width, question):
         random.shuffle(data)
 
         # keep track of previous weight
-        prev_w = w
+        prev_w = list(x[weight_value] for x in w)
 
         # loop through each data sample
         for x in copy.deepcopy(data): # copy is important, otherwise we're changing the actual data (fixed bug)
@@ -166,34 +169,20 @@ def nn(data, w, lr, t, a, nn_width, input_width, question):
             # include bias
             x = [1] + x[:-1]
 
-            #print(w)
+            # get weight gradient
             gradient = back_propagation(x, w, label, nn_width, input_width, question)
-            print(gradient)
 
-            return
-
-            N = len(data) # data size 
-
-     
-            # and change any 0s to -1, that way it's easier to compare with
-            y = -1.0 if y == 0 else y 
-            # because we have b folded in w, the last value should be 1 so it can be multiplied through and have the bias be included in the final prediction value
-            x[-1] = 1 
-
-            # find our prediction value by multiplying our weight vector with the data sample
-            wx = dot(w, x)
-
-            # take the derivative of the svm objective at the current w to be the gradient J^t(w)
-            # different sub-gradient values based on the prediction value
-            # if y * wx <= 1:
-            #     w = add(sub(w, scale((w_0 + [0]), r)), scale(x, r*c*N*y))
-            # else:
-            #     w[:-1] = scale(w[:-1], 1 - r) # don't update bias 
-            w = sub(w, scale())
+            # update weight with it
+            weight_values = list(x[weight_value] for x in w)
+            updated_weight_values = sub(weight_values, scale(gradient, r))
+            for i in range(len(w)):
+                w[i][weight_value] = updated_weight_values[i]
+            
+            #print(w)
 
         # Stop iterating when the length of the weight difference vector
         # (from the prev iteration) is less than the tolerance level
-        w_diff = sub(prev_w, w)
+        w_diff = sub(prev_w, list(x[weight_value] for x in w))
         tolerance = 10e-3
         if norm(w_diff) < tolerance:
             print("converged at epoch:", epoch)
@@ -201,7 +190,7 @@ def nn(data, w, lr, t, a, nn_width, input_width, question):
 
     return w
 
-def print_errors(learned_weight, data, test_data):
+def print_errors(learned_weight, data, test_data, num_of_layers, nn_width, input_width, question):
     # ------------------------
     # TRAINING AND TEST ERRORS
     # ------------------------
@@ -213,11 +202,13 @@ def print_errors(learned_weight, data, test_data):
         label = x[-1]
         # and change any 0s to -1, that way it's easier to compare with
         label = -1.0 if label == 0 else label 
-        # because we have b folded in w, the last value should be 1 so it can be multiplied through and have the bias be included in the final prediction value
-        x[-1] = 1
+        # include bias
+        x = [1] + x[:-1]
 
-        # find our prediction value by multiplying our weight vector with the data sample
-        prediction = dot(learned_weight, x)
+        # find our prediction
+        _, y = forward_pass(x, learned_weight, num_of_layers, nn_width, input_width, question)
+
+        prediction = -1 if y < 0 else 1
 
         # if misclassified, update our weight vector
         if label * prediction <= 0:
@@ -234,11 +225,13 @@ def print_errors(learned_weight, data, test_data):
         label = x[-1]
         # and change any 0s to -1, that way it's easier to compare with
         label = -1.0 if label == 0 else label 
-        # because we have b folded in w, the last value should be 1 so it can be multiplied through and have the bias be included in the final prediction value
-        x[-1] = 1
+        # include bias
+        x = [1] + x[:-1]
 
-        # find our prediction value by multiplying our weight vector with the data sample
-        prediction = dot(learned_weight, x)
+        # find our prediction
+        _, y = forward_pass(x, learned_weight, num_of_layers, nn_width, input_width, question)
+
+        prediction = -1 if y < 0 else 1
 
         # if misclassified, update our weight vector
         if label * prediction <= 0:
@@ -288,13 +281,14 @@ def main():
 
     #print(z_values)
 
-    # backpropagation - question a example
+    # backpropagation - question a) example
     
     if question == "bp":
         x = [1, 1, 1]
         label = 1
         nn_width = 2
-        back_propagation(x, weights, label, nn_width, question)
+        input_width = len(x)
+        back_propagation(x, weights, label, nn_width, input_width, question)
     
 
     # define the file paths for the training and test data
@@ -304,26 +298,23 @@ def main():
     # read the data from the file into a list
     data = read_file(train_file)
     test_data = read_file(test_file)
-    size = len(data[0])
 
     # setup init values
-    w = [0] * size # folded b into w
-    r = 0.0001 # learning rate
+    r = 0.001 # learning rate
     t = 100 # epoch
-    a = 0.0001 # for adjusting the learning rate
+    a = 0.001 # for adjusting the learning rate
     
-    
-    x = [1, 1, 1]
-    input_width = len(x)
+
+    num_of_layers = 3 
     nn_width = 2
-    num_of_layers = 3
+    input_width = len(data[0])
 
     # init edge weights based on standard gaussian distribution
     weights = []
     num_of_weights = input_width * nn_width * (num_of_layers - 1)
     for i in range(num_of_weights):
         layer = int(i / (input_width * nn_width))
-        node_from = int(i/2)  % input_width
+        node_from = int(i/2) % input_width
         node_to = i % nn_width + 1
         weight_value = gaussian_distrib(random.uniform(0, 1))
         weights.append([layer, node_from, node_to, weight_value])
@@ -333,13 +324,13 @@ def main():
         node_to = 1
         weight_value = gaussian_distrib(random.uniform(0, 1))
         weights.append([layer, node_from, node_to, weight_value])
-    
+
     # use the algorithm to calc the best weight vector
     learned_weight = nn(data, weights, r, t, a, nn_width, input_width, question)
-
-    #print("learned weight vector:", [round(num, 3) for num in learned_weight])
-
-    #print_errors(learned_weight, data, test_data)
+    learned_weight_values = list(x[3] for x in learned_weight)
+    print("learned weight vector:", [round(num, 3) for num in learned_weight_values])
+    
+    print_errors(learned_weight, data, test_data, num_of_layers, nn_width, input_width, question)
 
 
 
